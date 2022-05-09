aa802db834a35e84f5eee95c8f136d0eb57fbfc5
Adam Kwidzyński <adamkwidzynski@gmail.com>
2022-05-09 23:03:22 +0200

    add this

--- js/script.js
+++ js/script.js
@@ -8,11 +8,14 @@
   */
   
 const titleClickHandler = function(event){
+    const clickedElement = this;
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
     console.log('Link was clicked!');
     console.log("event = ", event);
 }
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -20,9 +23,12 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
   
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    
+    
   
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.posts .active');
 
